17f99510d4d4841f011c14cdaba8fab2420c4d46
Alexis <alexisurtecho.au@gmail.com>
2022-04-04 00:49:29 -0500

    refactor game initialization and split board renderization to a single controller action

--- app/controllers/api/v1/mine_sweeper_controller.rb
+++ app/controllers/api/v1/mine_sweeper_controller.rb
@@ -1,16 +1,26 @@
 module Api
     module V1
         class MineSweeperController < ApplicationController
+            before_action :set_game_state, only: [:set_position, :game_board]
+
             def start_game
                 game_state = GameState.create_game_state(mine_sweeper_params)
-                board_matrix = Minesweeper::GameStateRenderizer.new(game_state).render_board
-                render json: {board: board_matrix, game_state: game_state}
+                render json: {game_state: game_state}
             end
 
+            def game_board
+                board_matrix = Minesweeper::GameStateRenderizer.new(@game_state).render_board
+                render json: {board: board_matrix, game_state: @game_state}
+            end
 
-            #strong params for mine_sweeper_controller
+            private
+
             def mine_sweeper_params
                 params.require(:mine_sweeper).permit(:height, :width, :mines)
+            end
+
+            def set_game_state
+                @game_state = GameState.find(params[:game_state_id])
             end
         end
     end
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :mine_sweeper, only: [:start_game] do
+      resources :mine_sweeper, only: [:start_game, :game_board] do
         collection do
           post :start_game
+          get 'game_board/:game_state_id', to: 'mine_sweeper#game_board'
         end
       end
     end
