243c83cfc97f28a3cf9a21388b5fba5f77a442c4
Alexis <alexisurtecho.au@gmail.com>
2022-04-05 01:02:53 -0500

    update doc file

--- spec/requests/api/v1/mine_sweeper_spec.rb
+++ spec/requests/api/v1/mine_sweeper_spec.rb
@@ -18,4 +18,64 @@ RSpec.describe 'api/v1/mine_sweeper', type: :request do
       end
     end
   end
+
+  path '/api/v1/mine_sweeper/set_position/{game_state_id}' do
+    # You'll want to customize the parameter types...
+    parameter name: 'game_state_id', in: :path, type: :string, description: 'game_state_id'
+
+    post('set_position mine_sweeper') do
+      response(200, 'successful') do
+        let(:game_state_id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+
+  path '/api/v1/mine_sweeper/toggle_flag/{game_state_id}' do
+    # You'll want to customize the parameter types...
+    parameter name: 'game_state_id', in: :path, type: :string, description: 'game_state_id'
+
+    post('toggle_flag mine_sweeper') do
+      response(200, 'successful') do
+        let(:game_state_id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+
+  path '/api/v1/mine_sweeper/game_board/{game_state_id}' do
+    # You'll want to customize the parameter types...
+    parameter name: 'game_state_id', in: :path, type: :string, description: 'game_state_id'
+
+    get('game_board mine_sweeper') do
+      response(200, 'successful') do
+        let(:game_state_id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
 end
